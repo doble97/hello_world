edd5790e6744c3995f1d4afe72a01360c6f44b3f
Jorge Gonzalez <jorgegonzalez97d@gmail.com>
2022-04-22 13:47:16 +0200

    Clase

--- lib/servicios/callapi.dart
+++ lib/servicios/callapi.dart
@@ -1,20 +1,20 @@
-import 'dart:convert';
+// import 'dart:convert';
 
-import 'package:hello_world/entidades/response.dart';
-import 'package:http/http.dart' as http;
+// import 'package:hello_world/entidades/response.dart';
+// import 'package:http/http.dart' as http;
 
-class CallApi {
-  Future<ResponseApi> make_login(String email, String password) async {
-    var response = await http.post(Uri.parse('api' + 'login'),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8'
-        },
-        body:
-            jsonEncode(<String, String>{'email': email, 'password': password}));
-    if (response.statusCode == 200) {
-      return ResponseApi.fromJson(jsonDecode(response.body));
-    }else{
-      throw Exception('Fallo en obtener datos')
-    }
-  }
-}
+// class CallApi {
+//   Future<ResponseApi> make_login(String email, String password) async {
+//     var response = await http.post(Uri.parse('api' + 'login'),
+//         headers: <String, String>{
+//           'Content-Type': 'application/json; charset=UTF-8'
+//         },
+//         body:
+//             jsonEncode(<String, String>{'email': email, 'password': password}));
+//     if (response.statusCode == 200) {
+//       return ResponseApi.fromJson(jsonDecode(response.body));
+//     }else{
+//       throw Exception('Fallo en obtener datos')
+//     }
+//   }
+// }
